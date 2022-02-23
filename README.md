345b3bedb8f8cacd8e9f389949d3866b8aa05509
1ncursio <ckswn1323@naver.com>
2022-02-24 00:23:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# todolist-server
+
+## REST API
+
+---
+
+### GET /todos
+
+- 모든 todo를 반환
+- return type: `Todo[]`
+
+### GET /todos/:id
+
+- 지정된 id의 todo를 반환
+- return type: `Todo`
+
+### POST /todos
+
+- 새로운 todo를 생성하고, 생성된 todo를 반환
+- body 형식: `{ "content": "foo" }`
+- return type: `Todo`
+
+### PUT /todos/:id
+
+- 지정된 id의 todo를 수정하고, 수정된 todo를 반환
+- body 형식: `{ "content": "foo", "done": true }`
+  - `content` 나 `done` 둘 중 하나라도 없거나 타입이 맞지 않으면 `400 Bad Request`을 반환
+- return type: `Todo`
+
+### DELETE /todos/:id
+
+- 지정된 id의 todo를 삭제(`deletedAt`을 수정)하고, 삭제된 todo를 반환
+- return type: `Todo`
+
+## Interface
+
+---
+
+### Todo
+
+```typescript
+interface Todo {
+  id: number;
+  content: string;
+  done: boolean;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const todos = [
+  {
+    id: 1,
+    content: "Learn Node.js",
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: 2,
+    content: "Learn React.js",
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: 3,
+    content: "Learn Angular.js",
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    id: 4,
+    content: "Learn Vue.js",
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/todos", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "todos retrieved successfully",
+    payload: todos,
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+  if (!todo) {
+    return res.status(404).json({
+      status: 404,
+      message: "todo not found",
+      payload: null,
+    });
+  }
+  res.status(200).json({
+    status: 200,
+    message: "todo retrieved successfully",
+    payload: todo,
+  });
+});
+
+app.post("/todos", (req, res) => {
+  // validate body
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).json({
+      status: 400,
+      message: "content is required",
+      payload: null,
+    });
+  }
+
+  // create new todo
+  const todo = {
+    id: todos.length + 1,
+    content: req.body.content,
+    done: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  };
+
+  todos.push(todo);
+
+  res.status(201).json({
+    status: 201,
+    message: "todo created successfully",
+    payload: todo,
+  });
+});
+
+app.put("/todos/:id", (req, res) => {
+  // validate body
+  if (
+    !req.body.content ||
+    req.body.content.trim() === "" ||
+    typeof req.body.done !== "boolean"
+  ) {
+    return res.status(400).json({
+      status: 400,
+      message: "invalid request body",
+      payload: null,
+    });
+  }
+
+  const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+
+  if (!todo) {
+    return res.status(404).json({
+      status: 404,
+      message: "todo not found",
+      payload: null,
+    });
+  }
+
+  todo.content = String(req.body.content) ?? todo.content;
+  todo.done = Boolean(req.body.done) ?? todo.done;
+  todo.updatedAt = new Date();
+
+  res.status(200).json({
+    status: 200,
+    message: "todo updated successfully",
+    payload: todo,
+  });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+
+  if (!todo) {
+    return res.status(404).json({
+      status: 404,
+      message: "todo not found",
+      payload: null,
+    });
+  }
+
+  todo.deletedAt = new Date();
+
+  res.status(200).json({
+    status: 200,
+    message: "todo deleted successfully",
+    payload: todo,
+  });
+});
+
+app.listen(4444, () => {
+  console.log("Server started on port 4444");
+});
